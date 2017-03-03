b60e5da6fb91857f092f5d64b259deb0d9822b69
lmpynix <lmpynix@gmail.com>
2017-03-02 19:00:47 -0600

    The real initial commit

---
+++ src/SetupCV.cpp
@@ -0,0 +1,11 @@
+/* SetupCV.cpp
+ * This file is a component of the Rockford Robotics Vision Processing System.
+ * Copyright (c) 2017 Rockford Robotics, Inc.  All rights reserved.
+ * THIS SOFTWARE HAS NOT YET BEEN RELEASED.
+ *
+ * Original Author: Logan Power, 2017, for 2017(Zuko).
+ */
+
+
+
+
